329319283daa1d96991223f76522df11d7b627be
djibmsa@gmail.com <djibmsa@gmail.com>
2024-07-17 11:49:59 +0200

    contenu de la page acceuil faite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navabar/navbar";
+import Home from "./components/home/home";
 
 function App() {
   return (
     <main className="container">
       <Navbar />
+      <Home />
     </main>
   );
 }
